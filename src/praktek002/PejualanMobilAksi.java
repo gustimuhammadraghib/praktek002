28ef09cd4fa74670bf8345f6246d9eafa560b266
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-03-30 15:13:43 +0800

    Mengisi Nilai Attribute

--- src/praktek002/PejualanMobilAksi.java
+++ src/praktek002/PejualanMobilAksi.java
@@ -4,5 +4,8 @@ public class PejualanMobilAksi {
     public static void main(String[] args) {
         PenjualanMobil r1 = new PenjualanMobil();
         
+        r1.harga_mobil = 100000000;
+        r1.jumlah_unit = 2;
+        
     }
 }
