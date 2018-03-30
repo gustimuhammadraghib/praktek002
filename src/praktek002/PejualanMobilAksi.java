41811cea648686ddc421d8ea92e1dbf8039d4df9
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-03-30 15:10:35 +0800

    Instance Object

--- src/praktek002/PejualanMobilAksi.java
+++ src/praktek002/PejualanMobilAksi.java
@@ -1,14 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package praktek002;
 
-/**
- *
- * @author GMR
- */
 public class PejualanMobilAksi {
-    
+    public static void main(String[] args) {
+        PenjualanMobil r1 = new PenjualanMobil();
+        
+    }
 }
