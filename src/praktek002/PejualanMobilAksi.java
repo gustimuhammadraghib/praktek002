8f6eb29adafd0a67f927fc8103eb62ab49667c88
gustimuhammadraghib <gt.muhammadraghib@gmail.com>
2018-03-30 16:25:21 +0800

    Constructor Dengan Parameter

--- src/praktek002/PejualanMobilAksi.java
+++ src/praktek002/PejualanMobilAksi.java
@@ -7,8 +7,17 @@ public class PejualanMobilAksi {
         r1.harga_mobil = 100000000;
         r1.jumlah_unit = 2;
         
-        r1.cetakInfo();
-        System.out.println("total   =   "+r1.hitungtotal());
+        r1.tampildata();
+        System.out.println("total   =   "+r1.hitungPembelian());
         r1.cetaktotal();
+        
+        PenjualanMobil r2 = new PenjualanMobil();
+        r2.tampildata();
+        r2.cetaktotal();
+        
+        PenjualanMobil r3 = new PenjualanMobil(50000000,2);
+        r3.tampildata();
+        r3.cetaktotal();
+        
     }
 }
--- src/praktek002/PenjualanMobil.java
+++ src/praktek002/PenjualanMobil.java
@@ -1,29 +1,32 @@
 package praktek002;
-
- class PenjualanMobil {
-Integer harga_mobil;
-Integer jumlah_unit;
+        
+ public class PenjualanMobil {
+int harga_mobil;
+int jumlah_unit;
 
     public PenjualanMobil() {
      harga_mobil=70000000;
      jumlah_unit=2;
      
     }
-
-
- void cetakInfo(){
+    public PenjualanMobil(int harga_mobil, int jumlah_unit){
+        this.harga_mobil = harga_mobil;
+        this.jumlah_unit = jumlah_unit;
+    }
+    
+ void tampildata(){
     System.out.println("================================");
     System.out.println("harga_mobil     :"+harga_mobil);
     System.out.println("jumlah_unit     :"+jumlah_unit);
     System.out.println("================================");
  }
-    Integer hitungtotal () {
-            Integer total;
+    Integer hitungPembelian(){
+            int total;
             total=harga_mobil*jumlah_unit;
             return total;
             
     }
     void cetaktotal(){
-        System.out.println("Totalnya Adalah    = "+hitungtotal());
+        System.out.println("Totalnya    = "+hitungPembelian());
     }
  }
